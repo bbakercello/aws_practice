48947666a92d6ded5cc2172ad057e9b40c7e90d6
bbakercello <bbakercello@gmail.com>
2022-11-14 13:11:48 -0500

    index and event json configured for get route

--- amplify/backend/function/radioheadapi/src/index.py
+++ amplify/backend/function/radioheadapi/src/index.py
@@ -1,2 +1,17 @@
+import awsgi
 from flask_cors import CORS
-from flask import Flask
+from flask import Flask, jsonify, request
+
+BASE_ROUTE = '/song'
+
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route(BASE_ROUTE,methods=['GET'])
+def list_items():
+    return jsonify(message='hello world')
+
+def handler(event,context):
+    return awsgi.response(app,event,context)
